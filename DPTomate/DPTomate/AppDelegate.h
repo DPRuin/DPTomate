55bad67c6a4ee44b11d4f743282b8d88bedca1e7
DPRuin <dpruin@126.com>
2016-04-06 10:36:19 +0800

    初始化操作

---
+++ DPTomate/DPTomate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DPTomate
+//
+//  Created by 土老帽 on 16/4/6.
+//  Copyright © 2016年 DPRuin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DPTomate/DPTomate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DPTomate
+//
+//  Created by 土老帽 on 16/4/6.
+//  Copyright © 2016年 DPRuin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
